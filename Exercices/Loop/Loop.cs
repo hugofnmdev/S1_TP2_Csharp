d3f656c5e5a0f11e0caeff9f40db62d47e53dbf6
hugofnm <contact@hugofnm.fr>
2021-11-29 15:05:40 +0100

    Add files via upload

--- Exercices/Loop/Loop.cs
+++ Exercices/Loop/Loop.cs
@@ -7,6 +7,7 @@ namespace Exercice
         public static void PrintNaturals(int n)
         {
                 // TODO
+                Console.WriteLine(3);
         }
 
         public static void PrintPrimes(int n)
@@ -17,11 +18,13 @@ namespace Exercice
         public static long Fibonacci(long n)
         {
                 // TODO
+                return 0;
         }
 
         private static long Factorial(long n)
         {
                 // TODO
+                return 0;
         }
 
         public static void PrintStrong(int n)
@@ -32,16 +35,19 @@ namespace Exercice
         public static float Abs(float n)
         {
                 // TODO
+                return 0;
         }
 
         public static float Sqrt(float n)
         {
                 // TODO
+                return 0;
         }
 
         public static long Power(long a, long b)
         {
                 // TODO
+                return 0;
         }
 
         public static void PrintTree(int n)
@@ -52,6 +58,7 @@ namespace Exercice
         public static int Syracuse(int n)
         {
                 // TODO
+                return 0;
         }
 
     }
--- Exercices/Loop/Program.cs
+++ Exercices/Loop/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using Exercice;
 
 namespace Exercice
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            Loop.PrintNaturals(5);
+            // Leave it empty for the last push, use it to call and test your functions
+        }
         // you can test your functions here
     }
 }
